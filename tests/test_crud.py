01c67aa2bf0f8806e4c2409609d1ee8076cc4c53
RuthHuaman2026 <20241d022@khipu.edu.pe>
2026-02-27 17:57:30 -0500

    Fix tests import path

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,13 +1,22 @@
+import sys
+import os
+
+# Permite que pytest encuentre crud.py
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 import crud
+
 
 def test_create_and_read():
     crud.create("item1")
     assert "item1" in crud.read()
 
+
 def test_update():
     crud.create("item2")
     assert crud.update(0, "item2_updated")
     assert crud.read()[0] == "item2_updated"
+
 
 def test_delete():
     crud.create("item3")
