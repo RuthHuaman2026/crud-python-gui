75384fad8af48a310f01ef36bca8f1bb58c5f214
RuthHuaman2026 <20241d022@khipu.edu.pe>
2026-02-27 18:00:50 -0500

    Fix tests reset data

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,22 +1,16 @@
-import sys
-import os
-
-# Permite que pytest encuentre crud.py
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 import crud
 
+def setup_function():
+    crud.items.clear()
 
 def test_create_and_read():
     crud.create("item1")
     assert "item1" in crud.read()
 
-
 def test_update():
     crud.create("item2")
     assert crud.update(0, "item2_updated")
     assert crud.read()[0] == "item2_updated"
-
 
 def test_delete():
     crud.create("item3")
